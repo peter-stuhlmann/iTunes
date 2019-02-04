61a7bef42a9e1eb3ada3c2148f711b1de9d82367
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-02-05 00:06:15 +0100

    Added functions for audio preview

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,6 @@
 const trackList = music.map(track => {
     const index = music.indexOf(track) + 1
+    const preview = track.previewUrl
     const artworkUrl30 = track.artworkUrl30
     const collectionName = track.collectionName
     const trackName = track.trackName
@@ -7,21 +8,44 @@ const trackList = music.map(track => {
     let trackPrice = track.trackPrice
     let currency = track.currency
 
+    if (currency == `USD`) {
+        currency = "$"
+    }
+
     if (trackPrice == `-1`) {
         trackPrice = "Album only"
         currency = ""
     }
 
     const releaseDate = track.releaseDate.slice(0, 10)
+    
     return `
       <tr>
         <td>${index}</td>
+        <td>
+            <button id="play${index}" onclick="SoS_Play(${index})"><i class="fas fa-play"></i></button>
+            <button id="pause${index}" onclick="SoS_Pause(${index})"><i class="fas fa-pause"></i></button>
+            <audio id="player${index}" autoplay loop><source src="${preview}"></audio>
+        </td>
         <td><img alt="${collectionName}" title="${collectionName}" src="${artworkUrl30}"></td>
         <td>${trackName}</td>
         <td>${artistName}</td>
-        <td>${trackPrice} ${currency}</td>
-        <td>${releaseDate}</td>
+        <td class="nowrap">${releaseDate}</td>
+        <td>${trackPrice}&nbsp;${currency}</td>
       </tr>`
 })
+
+
+function SoS_Play(index) {
+    let player = document.querySelector(`#player${index}`);
+    let button = document.querySelector(`#play${index}`).style.color = "rgb(165, 165, 165)";
+    player.play();
+}
+
+function SoS_Pause(index) {
+    let player = document.querySelector(`#player${index}`);
+    let button = document.querySelector(`#play${index}`).style.color = "";
+    player.pause();
+}
 
 document.querySelector('.tracklist').innerHTML = trackList.join("")
