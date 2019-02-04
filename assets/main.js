185a90817d0981df615428840de22996edbe2868
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-02-04 18:37:59 +0100

    basic javascript code

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,21 @@
-console.log(music)
+const trackList = music.map(track => {
+    const index = music.indexOf(track) + 1
+    const artworkUrl30 = track.artworkUrl30
+    const collectionName = track.collectionName
+    const trackName = track.trackName
+    const artistName = track.artistName
+    const releaseDate = track.releaseDate.slice(0, 10)
+    const trackPrice = track.trackPrice
+    const currency = track.currency
+    return `
+      <tr>
+        <td>${index}</td>
+        <td><img alt="${collectionName}" title="${collectionName}" src="${artworkUrl30}"></td>
+        <td>${trackName}</td>
+        <td>${artistName}</td>
+        <td>${trackPrice} ${currency}</td>
+        <td>${releaseDate}</td>
+      </tr>`
+})
+
+document.querySelector('.tracklist').innerHTML = trackList.join("")
