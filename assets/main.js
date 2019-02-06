0457c85a013e98f7d7ca0a8b11db67552368031d
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-02-06 01:03:43 +0100

    Added filter function

--- assets/main.js
+++ assets/main.js
@@ -1,42 +1,57 @@
-const trackList = music.map(track => {
-    const index = music.indexOf(track) + 1
-    const preview = track.previewUrl
-    const artworkUrl30 = track.artworkUrl30
-    const collectionName = track.collectionName
-    const trackName = track.trackName
-    const artistName = track.artistName
-    let trackPrice = track.trackPrice
-    let currency = track.currency
+const renderTracks = (filterValue) => {
+    let arrayTracks = music;
 
-    if (currency == `USD`) {
-        currency = "$"
+    if (filterValue) {
+        arrayTracks = arrayTracks.filter(track => {
+            let {
+                artistName
+            } = track
+            artistName = artistName.toLowerCase()
+            filterValue = filterValue.toLowerCase()
+
+            return artistName.includes(filterValue)
+        })
     }
 
-    if (trackPrice == `-1`) {
-        trackPrice = "Album only"
-        currency = ""
-    }
+    const trackList = arrayTracks.map(track => {
+        const index = arrayTracks.indexOf(track) + 1
+        const preview = track.previewUrl
+        const artworkUrl30 = track.artworkUrl30
+        const collectionName = track.collectionName
+        const trackName = track.trackName
+        const artistName = track.artistName
+        let trackPrice = track.trackPrice
+        let currency = track.currency
 
-    const releaseDate = track.releaseDate.slice(0, 10)
+        if (currency == `USD`) {
+            currency = "$"
+        }
 
-    return `
+        if (trackPrice == `-1`) {
+            trackPrice = "Album only"
+            currency = ""
+        }
+
+        const releaseDate = track.releaseDate.slice(0, 10)
+
+        return `
       <tr>
         <td>${index}</td>
         <td>
             <button id="play${index}" onclick="SoS_Play(${index})"><i class="fas fa-play"></i></button>
             <button id="pause${index}" onclick="SoS_Pause(${index})"><i class="fas fa-pause"></i></button>
-            <audio id="player${index}" autoplay loop><source src="${preview}"></audio>
+            <audio id="player${index}" loop><source src="${preview}"></audio>
         </td>
         <td><img alt="${collectionName}" title="${collectionName}" src="${artworkUrl30}"></td>
         <td>${trackName}</td>
         <td>${artistName}</td>
-        <td>${trackPrice} ${currency}</td>
-        <td>${releaseDate}</td>
         <td class="nowrap">${releaseDate}</td>
         <td>${trackPrice}&nbsp;${currency}</td>
       </tr>`
-})
+    })
 
+    document.querySelector('.tracklist').innerHTML = trackList.join("")
+}
 
 function SoS_Play(index) {
     let player = document.querySelector(`#player${index}`);
@@ -50,4 +65,10 @@ function SoS_Pause(index) {
     player.pause();
 }
 
-document.querySelector('.tracklist').innerHTML = trackList.join("")
+renderTracks()
+
+const filterInput = document.querySelector("#filter-input")
+
+filterInput.onkeypress = function (e) {
+    renderTracks(filterInput.value)
+}
