aa54edad5f0961068c43dbe25899981687e5a276
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-02-13 21:58:52 +0100

    Prevented that multiple auditions can be played at the same time

--- assets/main.js
+++ assets/main.js
@@ -38,7 +38,7 @@ const renderTracks = (filterValue) => {
       <tr>
         <td>${index}</td>
         <td>
-            <button id="play${index}" onclick="SoS_Play(${index})"><i class="fas fa-play"></i></button>
+            <button id="play${index}" class="play" onclick="SoS_Play(${index})"><i class="fas fa-play"></i></button>
             <button id="pause${index}" onclick="SoS_Pause(${index})"><i class="fas fa-pause"></i></button>
             <audio id="player${index}" loop><source src="${preview}"></audio>
         </td>
@@ -54,9 +54,23 @@ const renderTracks = (filterValue) => {
 }
 
 function SoS_Play(index) {
+    let playerOff = document.querySelectorAll(`audio`);
+
+    for (let i = 0; i < playerOff.length; i++) {
+        playerOff[i].pause()
+    }
+
+    let buttonOff = document.querySelectorAll(`.play`);
+
+    for (let i = 0; i < buttonOff.length; i++) {
+        buttonOff[i].style.color = "rgb(165, 165, 165, .3)"
+    }
+
     let player = document.querySelector(`#player${index}`);
-    let button = document.querySelector(`#play${index}`).style.color = "rgb(165, 165, 165)";
+
     player.play();
+
+    let button = document.querySelector(`#play${index}`).style.color = "rgb(165, 165, 165)";
 }
 
 function SoS_Pause(index) {
@@ -75,69 +89,67 @@ filterInput.onkeyup = function (e) {
 
 
 
-
-
 // sort table (found at https://codepen.io/andrese52/pen/ZJENqp)
 
 function sortTable(n) {
-  var table,
-    rows,
-    switching,
-    i,
-    x,
-    y,
-    shouldSwitch,
-    dir,
-    switchcount = 0;
-  table = document.querySelector("table");
-  switching = true;
-  //Set the sorting direction to ascending:
-  dir = "asc";
-  /*Make a loop that will continue until
-  no switching has been done:*/
-  while (switching) {
-    //start by saying: no switching is done:
-    switching = false;
-    rows = table.getElementsByTagName("TR");
-    /*Loop through all table rows (except the
-    first, which contains table headers):*/
-    for (i = 1; i < rows.length - 1; i++) { //Change i=0 if you have the header th a separate table.
-      //start by saying there should be no switching:
-      shouldSwitch = false;
-      /*Get the two elements you want to compare,
-      one from current row and one from the next:*/
-      x = rows[i].getElementsByTagName("TD")[n];
-      y = rows[i + 1].getElementsByTagName("TD")[n];
-      /*check if the two rows should switch place,
-      based on the direction, asc or desc:*/
-      if (dir == "asc") {
-        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-          //if so, mark as a switch and break the loop:
-          shouldSwitch = true;
-          break;
+    var table,
+        rows,
+        switching,
+        i,
+        x,
+        y,
+        shouldSwitch,
+        dir,
+        switchcount = 0;
+    table = document.querySelector("table");
+    switching = true;
+    //Set the sorting direction to ascending:
+    dir = "asc";
+    /*Make a loop that will continue until
+    no switching has been done:*/
+    while (switching) {
+        //start by saying: no switching is done:
+        switching = false;
+        rows = table.getElementsByTagName("TR");
+        /*Loop through all table rows (except the
+        first, which contains table headers):*/
+        for (i = 1; i < rows.length - 1; i++) { //Change i=0 if you have the header th a separate table.
+            //start by saying there should be no switching:
+            shouldSwitch = false;
+            /*Get the two elements you want to compare,
+            one from current row and one from the next:*/
+            x = rows[i].getElementsByTagName("TD")[n];
+            y = rows[i + 1].getElementsByTagName("TD")[n];
+            /*check if the two rows should switch place,
+            based on the direction, asc or desc:*/
+            if (dir == "asc") {
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    //if so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            } else if (dir == "desc") {
+                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    //if so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            }
         }
-      } else if (dir == "desc") {
-        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-          //if so, mark as a switch and break the loop:
-          shouldSwitch = true;
-          break;
+        if (shouldSwitch) {
+            /*If a switch has been marked, make the switch
+            and mark that a switch has been done:*/
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            //Each time a switch is done, increase this count by 1:
+            switchcount++;
+        } else {
+            /*If no switching has been done AND the direction is "asc",
+            set the direction to "desc" and run the while loop again.*/
+            if (switchcount == 0 && dir == "asc") {
+                dir = "desc";
+                switching = true;
+            }
         }
-      }
     }
-    if (shouldSwitch) {
-      /*If a switch has been marked, make the switch
-      and mark that a switch has been done:*/
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-      //Each time a switch is done, increase this count by 1:
-      switchcount++;
-    } else {
-      /*If no switching has been done AND the direction is "asc",
-      set the direction to "desc" and run the while loop again.*/
-      if (switchcount == 0 && dir == "asc") {
-        dir = "desc";
-        switching = true;
-      }
-    }
-  }
 }
